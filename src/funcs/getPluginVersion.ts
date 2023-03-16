0ccb16fe4ac8879315a82a72f184715808caaa17
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-03-15 21:52:44 -0500

    add: pluginversion, queue entering and exiting

---
+++ src/funcs/getPluginVersion.ts
@@ -0,0 +1,7 @@
+function getPluginVersion() {
+  const manifest = require('../../public/manifest.json');
+  const { version } = manifest;
+  const { major, minor, patch } = version;
+  return `${major}.${minor}.${patch}`;
+}
+export { getPluginVersion };
