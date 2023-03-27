03a7f7c853ace10ee0b07f335cd28654ea65561a
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-03-27 00:54:30 -0500

    add: Idle Activity Support | version already bumped

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,18 +4,17 @@ import { sendPresence } from '../funcs/update_presence';
 import { getPluginVersion } from '../funcs/getPluginVersion';
 import { getHelperVersion } from '../funcs/getHelperVersion';
 
-// TODO: Create Real Idle Activity
-// all we do is keep track of the time between the last QueueCardComplete or the last EditorTextEdited (fires every time text is added 🔥) and IF remnotePluginAlive is true AND IF the time is greater than given time,	set the activity to idle
-
-// TODO: Create method that allows the first request from remnote that is not an activity to set the activity to idle,
-// but once that happens, we keep track of remnote session. If remnote session is true, then we don't set the activity to idle
-// then when we detect remnote is closed, we set the remnote session to false.
-
 const port = 3093;
-
+const DESIRED_VERSIION_HELPER = 'v0.0.6';
+let allowedIdleTime = 5; // in minutes
+let idleElapsedTime = new Date();
+let idleCheck: boolean;
 let elapsedTime = new Date();
 let aliveOrNah = { heartbeat: true };
 const pluginVersion = getPluginVersion();
+let inQueue: boolean = false;
+let justLeftQueue: boolean = false;
+
 function sendHeartbeat() {
   const myHeaders: HeadersInit = new Headers();
   myHeaders.append('Content-Type', 'application/json');
@@ -35,18 +34,37 @@ function sendHeartbeat() {
     .catch((error: Error): void => console.log('error', error));
 }
 
+function checkIdle() {
+  // check if the idleElapsed is greater than the plugin.settings idle-time
+  // get the idle-time by calling it from the plugin.settings
+  // if it is, then set the activity to idle
+
+  // convert allowedIdleTime to milliseconds from minutes
+  let tempTime = allowedIdleTime * 60000;
+
+  // we'll need to convert the number of minutes to milliseconds
+  // if idleElapsedTime is greater than 5 minutes
+  if (new Date().getTime() - idleElapsedTime.getTime() > tempTime) {
+    // set the activity to idle
+    setIdle();
+  }
+}
+
+// the below timeout-interval is a hacky trick to make a file run simliarly to a worker thread
 setTimeout(() => {
   setInterval(() => {
     sendHeartbeat();
   }, 5000);
 }, 25);
 
+setTimeout(() => {
+  setInterval(() => {
+    checkIdle();
+  }, 1000);
+}, 25);
+
 async function onActivate(plugin: ReactRNPlugin) {
-  await plugin.settings.registerBooleanSetting({
-    id: 'idle-check',
-    title: 'Should RemCord check for idle?',
-    defaultValue: true,
-  });
+  // send a heartbeat forcefully, and set the activity to idle (future TODO: doing nothing)
 
   await plugin.settings.registerStringSetting({
     id: 'editing-text',
@@ -54,29 +72,35 @@ async function onActivate(plugin: ReactRNPlugin) {
     defaultValue: 'Editing Rems',
   });
 
-  await plugin.settings.registerStringSetting({
-    id: 'viewing-text',
-    title: 'What should we show when viewing a document?',
-    defaultValue: 'Viewing {documentName}',
+  // await plugin.settings.registerStringSetting({
+  //   id: 'viewing-text',
+  //   title: 'What should we show when viewing a document?',
+  //   defaultValue: 'Viewing {documentName}',
+  // });
+
+  // // askuser if they want to show when they are studying their queue
+  // await plugin.settings.registerBooleanSetting({
+  //   id: 'show-queue',
+  //   title: 'Should RemCord show when you are studying your queue?',
+  //   defaultValue: true,
+  // });
+  // // ask user if they want to show their queue statistics
+  // await plugin.settings.registerBooleanSetting({
+  //   id: 'show-queue-stats',
+  //   title: 'Should RemCord show your queue statistics?',
+  //   defaultValue: true,
+  // });
+  await plugin.settings.registerBooleanSetting({
+    id: 'idle-check',
+    title: 'Should RemCord check for idle?',
+    defaultValue: true,
   });
 
   await plugin.settings.registerNumberSetting({
     id: 'idle-time',
     title: 'How long should we wait before considering you idle?',
-    defaultValue: 300,
-  });
-
-  // askuser if they want to show when they are studying their queue
-  await plugin.settings.registerBooleanSetting({
-    id: 'show-queue',
-    title: 'Should RemCord show when you are studying your queue?',
-    defaultValue: true,
-  });
-  // ask user if they want to show their queue statistics
-  await plugin.settings.registerBooleanSetting({
-    id: 'show-queue-stats',
-    title: 'Should RemCord show your queue statistics?',
-    defaultValue: true,
+    description: 'In minutes. Default is 5 minutes.',
+    defaultValue: 5,
   });
 
   // // A command that inserts text into the editor if focused.
@@ -96,6 +120,9 @@ async function onActivate(plugin: ReactRNPlugin) {
   // Defining listeners
   plugin.event.addListener(AppEvents.QueueEnter, undefined, async (data) => {
     setTimeout(async () => {
+      inQueue = true;
+      // update the idleElapsedTime
+      idleElapsedTime = new Date();
       // send a post request to the discord gateway
       sendPresence({
         details: 'Flashcard Queue',
@@ -113,6 +140,8 @@ async function onActivate(plugin: ReactRNPlugin) {
 
   plugin.event.addListener(AppEvents.QueueCompleteCard, undefined, async (data) => {
     setTimeout(async () => {
+      // update the idleElapsedTime
+      idleElapsedTime = new Date();
       // send a post request to the discord gateway
       sendPresence({
         details: 'Flashcard Queue',
@@ -130,34 +159,36 @@ async function onActivate(plugin: ReactRNPlugin) {
 
   plugin.event.addListener(AppEvents.QueueExit, undefined, async (data) => {
     setTimeout(async () => {
+      inQueue = false;
+      justLeftQueue = true;
       // send a post request to the discord gateway saying the user is idle
-
-      sendPresence({
-        details: 'Idle',
-        state: 'Not Studying',
-        largeImageKey: 'mocha_logo',
-        largeImageText: `RemCord v${pluginVersion}`,
-        smallImageKey: 'transparent_icon_logo',
-        // smallImageText: 'Maybe the Daily Goal can go here?',
-        startTimestamp: elapsedTime,
-        port: port,
-      });
+      setIdle();
     }, 25);
   });
 
   plugin.event.addListener(AppEvents.EditorTextEdited, undefined, async (data) => {
     setTimeout(async () => {
-      // send a post request to the discord gateway saying the user is editing!
-      sendPresence({
-        details: 'Rem Editor',
-        state: await plugin.settings.getSetting('editing-text'),
-        largeImageKey: 'mocha_logo',
-        largeImageText: `RemCord v${pluginVersion}`,
-        smallImageKey: 'transparent_icon_logo',
-        // smallImageText: 'Maybe the Daily Goal can go here?',
-        startTimestamp: elapsedTime,
-        port: port,
-      });
+      // update the idleElapsedTime
+      await setAsEditing(plugin, data);
+    }, 25);
+  });
+
+  plugin.event.addListener(AppEvents.FocusedRemChange, undefined, async (data) => {
+    setTimeout(async () => {
+      // update the idleElapsedTime
+      // if in queue, return
+      if (inQueue) return;
+      if (justLeftQueue) {
+        justLeftQueue = false;
+        return;
+      }
+      await setAsEditing(plugin, data);
+    }, 25);
+  });
+
+  plugin.event.addListener(AppEvents.SettingChanged, undefined, async (data) => {
+    setTimeout(async () => {
+      await pullSettings();
     }, 25);
   });
 
@@ -167,9 +198,57 @@ async function onActivate(plugin: ReactRNPlugin) {
   //   sendPresence({ destroy: true, port: port });
   // });
 
+  // pull settings from the plugin
+
+  await pullSettings();
+
   // Show a toast notification to the user.
-  await plugin.app.toast(`RemCord v${pluginVersion} Loaded!`);
-  await plugin.app.toast(`RemCord Helper ${await getHelperVersion()}`);
+  await plugin.app.toast(
+    `RemCord v${pluginVersion} Loaded!\nRemCord Helper ${await getHelperVersion()}`
+  );
+  if ((await getHelperVersion()) !== DESIRED_VERSIION_HELPER) {
+    await plugin.app.toast(
+      `RemCord Helper is out of date! Please update to ${DESIRED_VERSIION_HELPER}\nVisit the Github. Delete the old helper. \nDownload the New One!`
+    );
+  }
+
+  async function pullSettings() {
+    allowedIdleTime = await plugin.settings.getSetting('idle-time');
+    idleCheck = await plugin.settings.getSetting('idle-check');
+  }
+  sendHeartbeat();
+  setIdle();
+}
+
+async function setAsEditing(plugin: ReactRNPlugin, data?: any) {
+  idleElapsedTime = new Date();
+  // send a post request to the discord gateway saying the user is editing!
+  sendPresence({
+    details: 'Rem Editor',
+    state: await plugin.settings.getSetting('editing-text'),
+    largeImageKey: 'mocha_logo',
+    largeImageText: `RemCord v${pluginVersion}`,
+    smallImageKey: 'transparent_icon_logo',
+    // smallImageText: 'Maybe the Daily Goal can go here?',
+    startTimestamp: elapsedTime,
+    port: port,
+  });
+}
+
+function setIdle() {
+  if (!idleCheck) {
+    return;
+  }
+  sendPresence({
+    details: 'Idle',
+    state: 'Not Studying',
+    largeImageKey: 'mocha_logo',
+    largeImageText: `RemCord v${pluginVersion}`,
+    smallImageKey: 'transparent_icon_logo',
+    // smallImageText: 'Maybe the Daily Goal can go here?',
+    startTimestamp: elapsedTime,
+    port: port,
+  });
 }
 
 async function onDeactivate(_: ReactRNPlugin) {
